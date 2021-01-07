5148e17143a230023b744971d0ebbbb1986b6360
test <test@test.test>
2021-01-07 12:54:05 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker rm -f rinkeby-eth 
+
+docker run -itd --restart=unless-stopped -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --name rinkeby-eth -v $(pwd)/rinkeby:/root/.ethereum/rinkeby -p 8545:8545 -p 30303:30303 ethereum/client-go:v1.9.24 --rinkeby --rpcapi db,eth,net,web3,personal,web3,txpool,admin,miner --etherbase=0x95b194fa00e6ec9a35b1b483f404b6ca89603380 --syncmode=fast --rpc --rpcaddr 0.0.0.0 --cache 2048 --maxpeers 30 --allow-insecure-unlock
+
+
+docker logs -f rinkeby-eth --tail 10
